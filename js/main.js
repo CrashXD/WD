e8734cb9a351858f69a90e45e0b80774acfcdb0d
Crashxd <crashxd@inbox.ru>
2023-02-12 11:35:02 +0600

    js start

---
+++ js/main.js
@@ -0,0 +1,71 @@
+/* 
+    alert(2+2);
+//    3+3;
+*/
+// let a;
+// let b = 2, c, d, e;
+/*
+a = 23284732498 + 24329847329;
+console.log(a);
+a = 3;
+console.log(a);
+a = a + a * a - 100;
+console.log(a);
+a = 2939;
+a = a + a * a - 100;
+console.log(a);
+*/
+
+// const a = 3;
+// const pi = 3.14;
+
+// let a = prompt('Введите имя');
+// console.log(a);
+// alert(a);
+
+// let a = confirm("Согласен?");
+// console.log(a);
+// alert(a);
+
+/*
+let a = prompt('Введите число A');
+let b = prompt('Введите число B');
+
+let result = Number(a) + Number(b);
+
+alert(result);
+*/
+
+// let a = '';
+
+// ||
+// &&
+// !
+
+// if(confirm('Возьмешь кредит?') || confirm('Возьмешь рассрочку?')) {
+//     console.log('Ты в кабале!');
+// } else {
+//     console.log('Свобода');
+// }
+
+// if(confirm('Возьмешь яблоко?') && confirm('Возьмешь грушу?')) {
+//     console.log('Я объелся');
+// } else {
+//     console.log('Нормально');
+// }
+
+// if(!confirm('Зимой холодно?')) {
+//     console.log('Да, холодно');
+// } else {
+//     console.log('Нет, тепло');
+// }
+
+// if(confirm('Возьмешь яблоко?') && confirm('Возьмешь грушу?')) {
+//     if (2 + 2 > 5) {
+//         console.log('Я объелся');
+//     }
+// } else if (confirm('Мясо будешь?')) {
+//     console.log('Съел мясо');
+// } else {
+//     console.log('Ничего не съел');
+// }
